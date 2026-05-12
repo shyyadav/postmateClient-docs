9f7d53ba7ebbee2abb04418663d5d935d9822cca
Shyam Yadav <shyyadav401@gmail.com>
2026-05-11 21:37:54 -0500

    updated color

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -26,7 +26,10 @@ export default defineConfig({
     ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icon-16.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icon-32.png' }],
-
+// in .vitepress/config.ts head array:
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap' }],
     // iOS home screen
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
 
